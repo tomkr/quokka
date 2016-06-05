a1c17a95b247a6a6dace6ee4b6d77658b5d91e0f
Tom Kruijsen <tom@tomkruijsen.nl>
2016-06-05 09:36:45 +0200

    Add login dialog url function.
    This function constructs the login url that your app should redirect to so that the user can log in to Facebook and give your app permission.

--- lib/quokka.rb
+++ lib/quokka.rb
@@ -1,6 +1,26 @@
+require "addressable/template"
 require "quokka/version"
 
 # Tools for logging in with Facebook
 module Quokka
-  # Your code goes here...
+  LOGIN_URI = Addressable::Template.new(
+    "https://www.facebook.com/dialog/oauth{?q*}"
+  )
+
+  module_function
+
+  # Constructs a url that will open a login dialog on Facebook that requests
+  # permission for your app.
+  #
+  # @param [redirect_uri] URI which Facebook will redirect to after login.
+  #
+  # @return [String] URL to Facebook login dialog.
+  def login_dialog_url(redirect_uri)
+    LOGIN_URI.expand(
+      q: {
+        client_id: ENV.fetch("FACEBOOK_APP_ID"),
+        redirect_uri: redirect_uri
+      }
+    ).to_s
+  end
 end
--- spec/quokka_spec.rb
+++ spec/quokka_spec.rb
@@ -1,7 +1,23 @@
 require "spec_helper"
 
 describe Quokka do
+  let(:redirect_uri) { "http://www.example.com" }
+
+  before do
+    ENV["FACEBOOK_APP_ID"] = "app_id"
+  end
+
   it "has a version number" do
     expect(Quokka::VERSION).not_to be nil
+  end
+
+  describe "#login_dialog_url" do
+    it "creates the login dialog url" do
+      expect(described_class.login_dialog_url(redirect_uri))
+        .to eq(
+          "https://www.facebook.com/dialog/oauth?client_id=app_id" \
+          "&redirect_uri=http%3A%2F%2Fwww.example.com"
+        )
+    end
   end
 end
